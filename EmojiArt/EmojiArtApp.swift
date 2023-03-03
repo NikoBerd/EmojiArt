554ee3e859fabf76ad3e3780fc8ebc54add8df82
Niko Berdzenishvili <nikolozisia@gmail.com>
2023-03-03 20:08:43 +0400

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Niko on 03.03.23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
